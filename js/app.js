fbad9497b13bd1ae57c502eb04a1d3c9774d0bb6
David Rodriguez <david@bsdadm.com>
2020-11-19 14:59:14 -0500

    Clean up js file, set new colors and prep for mobile screens.

--- js/app.js
+++ js/app.js
@@ -6,29 +6,10 @@
  */
 
 // DOM Selectors
-const continueButton = document.getElementById('start-quiz-btn');
-const startButton = document.getElementById('start-btn');
-const quitButton = document.getElementById('quit-btn');
-const infoBox = document.querySelector('.info-box');
-const quizBox = document.getElementById('quiz-box');
-const Next = document.getElementById('Next');
+const startBtn = document.getElementById('startGame');
 
 // Event Listeners
 
-startButton.addEventListener('click', function() {
-    infoBox.style.display = "block";
-});
-
-quitButton.addEventListener('click', function() {
-    infoBox.style.display = "none";
-});
-
-continueButton.addEventListener('click', function() {
-    startButton.style.display = "none";
-    infoBox.style.display = "none";
-    quizBox.style.display = "block";
-});
-
-Next.addEventListener('click', function(e) {
-    e.preventDefault();
-});
+startBtn.addEventListener('click', function() {
+    startBtn.style.display = 'none';
+})
