b40be662ff6667c41baffe1bada316f6f025ab55
David Rodriguez <david@bsdadm.com>
2020-11-22 12:43:05 -0500

    added legal stuffs :)

--- js/questions.js
+++ js/questions.js
@@ -1,3 +1,10 @@
+/*
+ * App Name: Simple JS Quiz
+ * Author: David Saul Rodriguez II
+ * Copyright: Copyright 2020 David Rodriguez <david@bsdadm.com>
+ * License: BSD-2-Clause
+ */
+
 const questions = [
     {
         question: 'Which of the following statements are false for Javascript?',
